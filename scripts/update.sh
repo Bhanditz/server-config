7bbf64ee3604c1efd1c6557dfbd7e7fa0c4902c1
Dan Mills <code@sandmill.org>
2014-02-07 00:22:03 +0000

    small logging output fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,16 +2,16 @@
 
 if [[ $1 == "quick" ]]; then
     for i in staging testing legal-testing; do
-	echo "\nQuick update for: $i"
+	echo; echo "Quick update for: $i"
 	$HOME/$i.creativecommons.org/scripts/update.sh quick
     done
     exit
 fi
 
-echo "\nFull update for: staging"
+echo; echo "Full update for: staging"
 $HOME/staging.creativecommons.org/scripts/update.sh update-l10n
 
 for i in testing legal-testing; do
-    echo "\nFull update for: $i"
+    echo; echo "Full update for: $i"
     $HOME/$i.creativecommons.org/scripts/update.sh
 done
