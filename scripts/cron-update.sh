23f7d94a572ab5639a84b587687df2194e2cae9a
Dan Mills <code@sandmill.org>
2014-02-07 00:36:01 +0000

    fix bash output redirection to really be silent (to avoid cron mail)

--- scripts/cron-update.sh
+++ scripts/cron-update.sh
@@ -3,5 +3,5 @@
 TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${TOPDIR}"
 
-./self-update.sh 2>&1 >/dev/null
-./update.sh $* 2>&1 >/dev/null
+./self-update.sh >/dev/null 2>&1
+./update.sh $* >/dev/null 2>&1
