4fd2efa98d94073e6b5ae2a3736aa17a0a157178
Dan Mills <code@sandmill.org>
2014-02-19 00:08:00 +0000

    Add testing1-5 servers

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# all servers except staging (which updates l10n so is separate)
+servers="legal-testing testing testing1 testing2 testing3 testing4 testing5"
+
 if [[ $1 == "quick" ]]; then
-    for i in staging testing legal-testing; do
+    for i in staging ${servers}; do
 	echo; echo "Quick update for: $i"
 	$HOME/$i.creativecommons.org/scripts/update.sh quick
     done
@@ -11,7 +14,7 @@ fi
 echo; echo "Full update for: staging"
 $HOME/staging.creativecommons.org/scripts/update.sh update-l10n
 
-for i in testing legal-testing; do
+for i in ${servers}; do
     echo; echo "Full update for: $i"
     $HOME/$i.creativecommons.org/scripts/update.sh
 done
